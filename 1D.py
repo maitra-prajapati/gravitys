d29f7f9aa185a27e4a0a011e08cc7e3fddc3e194
anuragpriyams-beep <anuragpriyams@gmail.com>
2025-12-18 09:42:43 +0530

    Update and rename 1D to 1D.py

---
+++ 1D.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class PhysicsObject:
+  def __init__(self, mass=1.0, position=np.array([0.0, 0.0]), 
+                 velocity=np.array([0.0, 0.0]),
+                 acceleration=np.array([0.0, 0.0])):
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.acceleration = acceleration
+                   
+   def _linear_mov(self,dt):
+     """Euler integration: v += a*dt, x += v*dt"""
+        self.acceleration = (self.net_force + self.mass * self.gravity) / self.mass
+        self.velocity += self.acceleration * dt
+        self.position += self.velocity * dt
+
+   def reset_forces(self):
+        self.net_force = np.zeros(2)
+
+   def apply_force(self,force)
+"""Apply force at a point"""
+       self.net_force += force
