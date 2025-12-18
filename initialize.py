5b4028a5d8a0108fae0ba6a3cce961dfce199f6b
MAITRA PRAJAPATI <maitra3018@gmail.com>
2025-12-18 22:39:09 +0530

    modified:   initialize.py

--- initialize.py
+++ initialize.py
@@ -1,24 +1,29 @@
 import numpy as np
 
 class PhysicsObject:
-    def __init__(self, mass=1.0, position=np.array([0.0, 0.0]), 
-                 velocity=np.array([0.0, 0.0]),
-                 acceleration=np.array([0.0, 0.0]),
-                 gravity=np.array([0.0, -9.81]), 
-                 angular_velocity=0.0, 
-                 angular_acceleration=0.0, 
-                 net_force=np.array([0.0, 0.0]), 
+    def __init__(self,
+                 mass=1.0,
+                 position=None,
+                 velocity=None,
+                 acceleration=None,
+                 gravity=None,
+                 angular_velocity=0.0,
+                 angular_acceleration=0.0,
+                 net_force=None,
                  net_torque=0.0):
+
         self.mass = mass
         self.position = np.array(position if position is not None else [0.0, 0.0], dtype = float)
         self.velocity = np.array(velocity if velocity is not None else [0.0, 0.0], dtype = float)
         self.acceleration = np.array(acceleration if acceleration is not None else [0.0, 0.0], dtype = float)
-        self.gravity = gravity
-        self.angular_velocity = np.array(angular_velocity if angular_velocity is not None else [0.0, 0.0], dtype = float)
-        self.angular_acceleration =np.array(angular_acceleration if angular_acceleration is not None else [0.0, 0.0], dtype = float)
+        self.gravity = np.array(gravity if gravity is not None else [0.0, -9.81], dtype=float)
         self.net_force = np.array(net_force if net_force is not None else [0.0, 0.0], dtype = float)
-        self.net_torque =np.array(net_torque if net_torque is not None else [0.0, 0.0], dtype = float) # Scalar for 2D rotation around z-axis
-    
+
+        self.angular_velocity = float(angular_velocity)
+        self.angular_acceleration = float(angular_acceleration)
+        self.net_torque = float(net_torque)
+
+
     def update_linear(self, dt):
         """Euler integration: v += a*dt, x += v*dt"""
         self.acceleration = (self.net_force + self.mass * self.gravity) / self.mass
